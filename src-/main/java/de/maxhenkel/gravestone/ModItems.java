99d246fdf39f51482a7194f8842f0fd8959fcc14
Max Henkel <max@maxhenkel.de>
2017-06-28 16:44:02 +0200

    removed unused

--- src-/main/java/de/maxhenkel/gravestone/ModItems.java
+++
@@ -1,7 +0,0 @@
-package de.maxhenkel.gravestone;
-
-import de.maxhenkel.gravestone.items.ItemDeathInfo;
-
-public class ModItems {
-	public static final ItemDeathInfo DEATH_INFO=new ItemDeathInfo();
-}
