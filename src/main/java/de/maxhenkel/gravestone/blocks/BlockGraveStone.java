c4d02cc8bb5f5700240ed9057795b59902f26ea3
Max Henkel <max@maxhenkel.de>
2018-01-27 19:54:14 +0100

    Fix snow layer can be placed on top of grave

--- src/main/java/de/maxhenkel/gravestone/blocks/BlockGraveStone.java
+++ src/main/java/de/maxhenkel/gravestone/blocks/BlockGraveStone.java
@@ -11,6 +11,7 @@ import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -36,185 +37,194 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockGraveStone extends BlockContainer {
 
-	public static final String NAME = "gravestone";
+    public static final String NAME = "gravestone";
 
-	public static final PropertyDirection FACING = PropertyDirection.create("facing", Collections2.filter(Lists.newArrayList(EnumFacing.values()), EnumFacing.Plane.HORIZONTAL));
+    public static final PropertyDirection FACING = PropertyDirection.create("facing", Collections2.filter(Lists.newArrayList(EnumFacing.values()), EnumFacing.Plane.HORIZONTAL));
 
-	public BlockGraveStone() {
-		super(new Material(MapColor.DIRT));
+    public BlockGraveStone() {
+        super(new Material(MapColor.DIRT));
 
-		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
-		this.setUnlocalizedName(NAME);
-		this.setRegistryName(Main.MODID, NAME);
-		this.setCreativeTab(CreativeTabs.DECORATIONS);
-		this.setHardness(0.3F);
-		this.setResistance(Float.MAX_VALUE);
-		this.useNeighborBrightness = true;
-	}
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+        this.setUnlocalizedName(NAME);
+        this.setRegistryName(Main.MODID, NAME);
+        this.setCreativeTab(CreativeTabs.DECORATIONS);
+        this.setHardness(0.3F);
+        this.setResistance(Float.MAX_VALUE);
+        this.useNeighborBrightness = true;
+    }
 
-	@Override
-	public void onBlockDestroyedByExplosion(World worldIn, BlockPos pos, Explosion explosionIn) {
+    @Override
+    public void onBlockDestroyedByExplosion(World worldIn, BlockPos pos, Explosion explosionIn) {
 
-	}
+    }
 
-	@Override
-	public void onBlockExploded(World world, BlockPos pos, Explosion explosion) {
+    @Override
+    public void onBlockExploded(World world, BlockPos pos, Explosion explosion) {
 
-	}
+    }
 
-	@Override
-	public boolean isBlockNormalCube(IBlockState state) {
-		return false;
-	}
+    @Override
+    public boolean isBlockNormalCube(IBlockState state) {
+        return false;
+    }
 
-	@Override
-	public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face) {
-		return false;
-	}
+    @Override
+    public boolean doesSideBlockRendering(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face) {
+        return false;
+    }
 
-	@Override
-	public IBlockState getStateFromMeta(int meta) {
-		EnumFacing enumFacing = EnumFacing.getFront(meta);
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        EnumFacing enumFacing = EnumFacing.getFront(meta);
 
-		if (enumFacing.getAxis() == EnumFacing.Axis.Y) {
-			enumFacing = EnumFacing.NORTH;
-		}
+        if (enumFacing.getAxis() == EnumFacing.Axis.Y) {
+            enumFacing = EnumFacing.NORTH;
+        }
 
-		return this.getDefaultState().withProperty(FACING, enumFacing);
-	}
+        return this.getDefaultState().withProperty(FACING, enumFacing);
+    }
 
-	@Override
-	public int getMetaFromState(IBlockState state) {
-		return ((EnumFacing) state.getValue(FACING)).getIndex();
-	}
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing) state.getValue(FACING)).getIndex();
+    }
 
-	@Override
-	protected BlockStateContainer createBlockState() {
-		return new BlockStateContainer(this, new IProperty[] { FACING });
-	}
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[]{FACING});
+    }
 
-	@Override
-	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
-			float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
-		return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing());
-	}
+    @Override
+    public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+                                            float hitZ, int meta, EntityLivingBase placer, EnumHand hand) {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing());
+    }
 
-	@Override
-	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
-		return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 1.0F, 0.11F, 1.0F);
-	}
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return new AxisAlignedBB(0.0F, 0.0F, 0.0F, 1.0F, 0.11F, 1.0F);
+    }
 
-	@Override
-	public boolean isFullBlock(IBlockState state) {
-		return false;
-	}
+    @Override
+    public boolean isFullBlock(IBlockState state) {
+        return false;
+    }
 
-	@Override
-	public boolean isFullCube(IBlockState state) {
-		return false;
-	}
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
 
-	@Override
-	public boolean isNormalCube(IBlockState state) {
-		return false;
-	}
+    @Override
+    public boolean isNormalCube(IBlockState state) {
+        return false;
+    }
 
-	@Override
-	public boolean isNormalCube(IBlockState state, IBlockAccess world, BlockPos pos) {
-		return false;
-	}
+    @Override
+    public boolean isNormalCube(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return false;
+    }
 
-	@Override
-	public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
-		return false;
-	}
+    @Override
+    public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
+        return false;
+    }
 
-	@Override
-	public TileEntity createNewTileEntity(World worldIn, int meta) {
-		return new TileEntityGraveStone();
-	}
+    @Override
+    public TileEntity createNewTileEntity(World worldIn, int meta) {
+        return new TileEntityGraveStone();
+    }
 
-	@Override
-	public EnumBlockRenderType getRenderType(IBlockState state) {
-		return EnumBlockRenderType.MODEL;
-	}
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state) {
+        return EnumBlockRenderType.MODEL;
+    }
 
-	@SideOnly(Side.CLIENT)
-	public BlockRenderLayer getBlockLayer() {
-		return BlockRenderLayer.CUTOUT;
-	}
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
 
-	@Override
-	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
-		TileEntity tileentity = worldIn.getTileEntity(pos);
+    @Override
+    public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+        TileEntity tileentity = worldIn.getTileEntity(pos);
 
-		if (tileentity instanceof TileEntityGraveStone) {
-			InventoryHelper.dropInventoryItems(worldIn, pos, (TileEntityGraveStone) tileentity);
-			worldIn.updateComparatorOutputLevel(pos, this);
-		}
+        if (tileentity instanceof TileEntityGraveStone) {
+            InventoryHelper.dropInventoryItems(worldIn, pos, (TileEntityGraveStone) tileentity);
+            worldIn.updateComparatorOutputLevel(pos, this);
+        }
 
-		super.breakBlock(worldIn, pos, state);
-	}
+        super.breakBlock(worldIn, pos, state);
+    }
 
-	@Override
-	public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
-		return true;
-	}
+    @Override
+    public boolean canSilkHarvest(World world, BlockPos pos, IBlockState state, EntityPlayer player) {
+        return true;
+    }
 
-	@Override
-	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-		return (Item) null;
-	}
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return (Item) null;
+    }
 
-	@Override
-	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
-			EnumHand hand, EnumFacing heldItem, float side, float hitX, float hitY) {
-		if (worldIn.isRemote) {
-			return true;
-		}
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+                                    EnumHand hand, EnumFacing heldItem, float side, float hitX, float hitY) {
+        if (worldIn.isRemote) {
+            return true;
+        }
 
-		TileEntity tileentity = worldIn.getTileEntity(pos);
+        TileEntity tileentity = worldIn.getTileEntity(pos);
 
-		if (!(tileentity instanceof TileEntityGraveStone)) {
-			return super.onBlockActivated(worldIn, pos, state, playerIn, hand, heldItem, side, hitX, hitY);
-		}
+        if (!(tileentity instanceof TileEntityGraveStone)) {
+            return super.onBlockActivated(worldIn, pos, state, playerIn, hand, heldItem, side, hitX, hitY);
+        }
 
-		TileEntityGraveStone grave = (TileEntityGraveStone) tileentity;
+        TileEntityGraveStone grave = (TileEntityGraveStone) tileentity;
 
-		return displayGraveInfo(grave, playerIn);
+        return displayGraveInfo(grave, playerIn);
 
-	}
+    }
 
-	private boolean displayGraveInfo(TileEntityGraveStone grave, EntityPlayer player) {
-		String name = grave.getPlayerName();
-		String time = grave.getTimeString();
+    private boolean displayGraveInfo(TileEntityGraveStone grave, EntityPlayer player) {
+        String name = grave.getPlayerName();
+        String time = grave.getTimeString();
 
-		if (name == null || name.isEmpty()) {
-			return true;
-		}
+        if (name == null || name.isEmpty()) {
+            return true;
+        }
 
-		if (time == null || time.isEmpty()) {
-			player.sendMessage(new TextComponentString(name));
-		} else {
-			player.sendMessage(new TextComponentTranslation("message.died", name, time));
-		}
+        if (time == null || time.isEmpty()) {
+            player.sendMessage(new TextComponentString(name));
+        } else {
+            player.sendMessage(new TextComponentTranslation("message.died", name, time));
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos) {
-		return false;
-	}
+    @Override
+    public boolean isReplaceable(IBlockAccess worldIn, BlockPos pos) {
+        return false;
+    }
 
-	@Override
-	public boolean canPlaceTorchOnTop(IBlockState state, IBlockAccess world, BlockPos pos) {
-		return false;
-	}
+    @Override
+    public boolean canPlaceTorchOnTop(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return false;
+    }
 
-	@Override
-	public boolean canBeReplacedByLeaves(IBlockState state, IBlockAccess world, BlockPos pos) {
-		return false;
-	}
+    @Override
+    public boolean canBeReplacedByLeaves(IBlockState state, IBlockAccess world, BlockPos pos) {
+        return false;
+    }
 
+    @Override
+    public boolean isTopSolid(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+        return face == EnumFacing.DOWN ? BlockFaceShape.SOLID : BlockFaceShape.UNDEFINED;
+    }
 }
