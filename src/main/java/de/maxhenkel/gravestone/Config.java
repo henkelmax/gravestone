266a0202aba368fc4627aaa8178b6d14fbed383e
Max Henkel <max@maxhenkel.de>
2018-10-08 21:53:15 +0200

    Add friendly ghost config option

--- src/main/java/de/maxhenkel/gravestone/Config.java
+++ src/main/java/de/maxhenkel/gravestone/Config.java
@@ -12,144 +12,151 @@ import net.minecraftforge.common.config.Configuration;
 
 public class Config {
 
-	public static Map<Integer, String> dimensionNames = new HashMap<Integer, String>();
-	public static String dateFormat = new String();
-	public static boolean renderSkull = true;
-	public static boolean livingGraves = false;
-	public static boolean giveDeathNotes = true;
-	public static List<Block> replaceableBlocks = new ArrayList<Block>();
-	public static boolean removeDeathNote = false;
-	public static boolean onlyPlayersCanBreak = false;
-	public static boolean spawnGhost = false;
+    public static Map<Integer, String> dimensionNames = new HashMap<Integer, String>();
+    public static String dateFormat = new String();
+    public static boolean renderSkull = true;
+    public static boolean livingGraves = false;
+    public static boolean giveDeathNotes = true;
+    public static List<Block> replaceableBlocks = new ArrayList<Block>();
+    public static boolean removeDeathNote = false;
+    public static boolean onlyPlayersCanBreak = false;
+    public static boolean spawnGhost = false;
+    public static boolean friendlyGhost = true;
 
-	public static void init(Configuration config) {
-		config.load();
+    public static void init(Configuration config) {
+        config.load();
 
-		dimensionNames = Collections.unmodifiableMap(getDimensionNames(config));
-		dateFormat = getDateFormat(config);
-		renderSkull = renderSkull(config);
-		livingGraves = livingGraves(config);
-		giveDeathNotes = giveDeathNotes(config);
-		replaceableBlocks = Collections.unmodifiableList(getReplaceableBlocks(config));
-		removeDeathNote = removeDeathNote(config);
-		onlyPlayersCanBreak = onlyOwnersCanBreak(config);
-		spawnGhost = spawnGhost(config);
+        dimensionNames = Collections.unmodifiableMap(getDimensionNames(config));
+        dateFormat = getDateFormat(config);
+        renderSkull = renderSkull(config);
+        livingGraves = livingGraves(config);
+        giveDeathNotes = giveDeathNotes(config);
+        replaceableBlocks = Collections.unmodifiableList(getReplaceableBlocks(config));
+        removeDeathNote = removeDeathNote(config);
+        onlyPlayersCanBreak = onlyOwnersCanBreak(config);
+        spawnGhost = spawnGhost(config);
+        friendlyGhost = friendlyGhost(config);
 
-		config.save();
-	}
+        config.save();
+    }
 
-	/*
-	 * private static String[] getStringArray(Configuration config, String key,
-	 * String category, String[] defaultValues, String comment) { String[] s =
-	 * defaultValues; try { s = config.getStringList(key, category,
-	 * defaultValues, comment); } catch (Exception e) { } return s; }
-	 */
+    /*
+     * private static String[] getStringArray(Configuration config, String key,
+     * String category, String[] defaultValues, String comment) { String[] s =
+     * defaultValues; try { s = config.getStringList(key, category,
+     * defaultValues, comment); } catch (Exception e) { } return s; }
+     */
 
-	private static Map<Integer, String> getDimensionNames(Configuration config) {
-		String[] def = new String[] { "-1: Nether", "0: Overworld", "1: The End" };
+    private static Map<Integer, String> getDimensionNames(Configuration config) {
+        String[] def = new String[]{"-1: Nether", "0: Overworld", "1: The End"};
 
-		String[] dimsStr = config.getStringList("dimension_names", "gravestone", def,
-				"The names of the Dimensions for the Death Note");
+        String[] dimsStr = config.getStringList("dimension_names", "gravestone", def,
+                "The names of the Dimensions for the Death Note");
 
-		Map<Integer, String> dims = new HashMap<Integer, String>();
+        Map<Integer, String> dims = new HashMap<Integer, String>();
 
-		for (String str : dimsStr) {
-			try {
-				int i = str.indexOf(":");
+        for (String str : dimsStr) {
+            try {
+                int i = str.indexOf(":");
 
-				if (i < 0) {
-					continue;
-				}
+                if (i < 0) {
+                    continue;
+                }
 
-				if (str.length() - 1 < i + 1) {
-					continue;
-				}
+                if (str.length() - 1 < i + 1) {
+                    continue;
+                }
 
-				String did = str.substring(0, i);
-				String name = str.substring(i + 1).trim();
-				int dimid = 0;
+                String did = str.substring(0, i);
+                String name = str.substring(i + 1).trim();
+                int dimid = 0;
 
-				try {
-					dimid = Integer.parseInt(did);
-				} catch (NumberFormatException e) {
-					Log.w("Failed to parse dimension ID: " + e.getMessage());
-					continue;
-				}
+                try {
+                    dimid = Integer.parseInt(did);
+                } catch (NumberFormatException e) {
+                    Log.w("Failed to parse dimension ID: " + e.getMessage());
+                    continue;
+                }
 
-				if (name == null || name.isEmpty()) {
-					Log.w("Failed to load dimension name for id " + dimid);
-					continue;
-				}
+                if (name == null || name.isEmpty()) {
+                    Log.w("Failed to load dimension name for id " + dimid);
+                    continue;
+                }
 
-				dims.put(dimid, name);
-			} catch (Exception e) {
-				Log.w("Failed to load dimension name '" + str + "': " + e.getMessage());
-			}
-		}
+                dims.put(dimid, name);
+            } catch (Exception e) {
+                Log.w("Failed to load dimension name '" + str + "': " + e.getMessage());
+            }
+        }
 
-		return dims;
-	}
+        return dims;
+    }
 
-	private static String getDateFormat(Configuration config) {
-		return config.getString("grave_date_format", "gravestone", "yyyy/MM/dd HH:mm:ss",
-				"The date format outputted by clicking the gravestone or displayed in the death note");
-	}
+    private static String getDateFormat(Configuration config) {
+        return config.getString("grave_date_format", "gravestone", "yyyy/MM/dd HH:mm:ss",
+                "The date format outputted by clicking the gravestone or displayed in the death note");
+    }
 
-	private static boolean renderSkull(Configuration config) {
-		return config.getBoolean("render_skull", "gravestone", true,
-				"If this is set to true the players head will be rendered on the gravestone when there is a full block under it");
-	}
+    private static boolean renderSkull(Configuration config) {
+        return config.getBoolean("render_skull", "gravestone", true,
+                "If this is set to true the players head will be rendered on the gravestone when there is a full block under it");
+    }
 
-	private static boolean livingGraves(Configuration config) {
-		return config.getBoolean("enable_living_entity_graves", "gravestone", false,
-				"If this is set to true every living entity will generate a gravestone");
-	}
+    private static boolean livingGraves(Configuration config) {
+        return config.getBoolean("enable_living_entity_graves", "gravestone", false,
+                "If this is set to true every living entity will generate a gravestone");
+    }
 
-	private static boolean giveDeathNotes(Configuration config) {
-		return config.getBoolean("enable_death_note", "gravestone", true,
-				"If this is set to true you get a death note after you died");
-	}
+    private static boolean giveDeathNotes(Configuration config) {
+        return config.getBoolean("enable_death_note", "gravestone", true,
+                "If this is set to true you get a death note after you died");
+    }
 
-	private static final String[] DEFAULT_BLOCKS = new String[] { "minecraft:tallgrass", "minecraft:water",
-			"minecraft:lava", "minecraft:yellow_flower", "minecraft:red_flower", "minecraft:double_plant",
-			"minecraft:sapling", "minecraft:brown_mushroom", "minecraft:red_mushroom", "minecraft:torch",
-			"minecraft:snow_layer", "minecraft:vine", "minecraft:deadbush", "minecraft:reeds", "minecraft:fire" };
+    private static final String[] DEFAULT_BLOCKS = new String[]{"minecraft:tallgrass", "minecraft:water",
+            "minecraft:lava", "minecraft:yellow_flower", "minecraft:red_flower", "minecraft:double_plant",
+            "minecraft:sapling", "minecraft:brown_mushroom", "minecraft:red_mushroom", "minecraft:torch",
+            "minecraft:snow_layer", "minecraft:vine", "minecraft:deadbush", "minecraft:reeds", "minecraft:fire"};
 
-	private static List<Block> getReplaceableBlocks(Configuration config) {
-		List<Block> replaceableBlocks = new ArrayList<Block>();
-		try {
-			String[] blocks = config.getStringList("replaceable_blocks", "gravestone", DEFAULT_BLOCKS, "The blocks that can be replaced with a grave when someone dies on it");
+    private static List<Block> getReplaceableBlocks(Configuration config) {
+        List<Block> replaceableBlocks = new ArrayList<Block>();
+        try {
+            String[] blocks = config.getStringList("replaceable_blocks", "gravestone", DEFAULT_BLOCKS, "The blocks that can be replaced with a grave when someone dies on it");
 
-			replaceableBlocks = Tools.getBlocks(blocks);
+            replaceableBlocks = Tools.getBlocks(blocks);
 
-			if (blocks == null) {
-				throw new IllegalArgumentException();
-			}
-		} catch (Exception e) {
-			Log.w("Failed to load replaceable blocks");
-			try {
-				replaceableBlocks = Tools.getBlocks(DEFAULT_BLOCKS);
-			} catch (Exception ex) {
-				replaceableBlocks = new ArrayList<Block>();
-			}
-		}
+            if (blocks == null) {
+                throw new IllegalArgumentException();
+            }
+        } catch (Exception e) {
+            Log.w("Failed to load replaceable blocks");
+            try {
+                replaceableBlocks = Tools.getBlocks(DEFAULT_BLOCKS);
+            } catch (Exception ex) {
+                replaceableBlocks = new ArrayList<Block>();
+            }
+        }
 
-		return replaceableBlocks;
-	}
+        return replaceableBlocks;
+    }
 
-	private static boolean removeDeathNote(Configuration config) {
-		return config.getBoolean("remove_death_note", "gravestone", false,
-				"If this is set to true the death note will be taken out of your inventory when you destroyed the gravestone");
-	}
+    private static boolean removeDeathNote(Configuration config) {
+        return config.getBoolean("remove_death_note", "gravestone", false,
+                "If this is set to true the death note will be taken out of your inventory when you destroyed the gravestone");
+    }
 
-	private static boolean onlyOwnersCanBreak(Configuration config) {
-		return config.getBoolean("only_owners_can_break", "gravestone", false,
-				"If this is set to true only the player that owns the gravestone and the admins can break the gravestone");
-	}
+    private static boolean onlyOwnersCanBreak(Configuration config) {
+        return config.getBoolean("only_owners_can_break", "gravestone", false,
+                "If this is set to true only the player that owns the gravestone and the admins can break the gravestone");
+    }
 
-	private static boolean spawnGhost(Configuration config) {
-		return config.getBoolean("spawn_ghost", "gravestone", false,
-				"If this is set to true a ghost of the dead player will be spawned when the gravestone is broken");
-	}
+    private static boolean spawnGhost(Configuration config) {
+        return config.getBoolean("spawn_ghost", "gravestone", false,
+                "If this is set to true a ghost of the dead player will be spawned when the gravestone is broken");
+    }
+
+    private static boolean friendlyGhost(Configuration config) {
+        return config.getBoolean("friendly_ghost", "gravestone", true,
+                "If this is set to true the ghost of the dead player will defend him");
+    }
 
 }
--- src/main/java/de/maxhenkel/gravestone/entity/EntityGhostPlayer.java
+++ src/main/java/de/maxhenkel/gravestone/entity/EntityGhostPlayer.java
@@ -2,7 +2,11 @@ package de.maxhenkel.gravestone.entity;
 
 import java.util.UUID;
 import javax.annotation.Nullable;
+
+import com.google.common.base.Predicate;
+import de.maxhenkel.gravestone.Config;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
@@ -12,7 +16,9 @@ import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
@@ -23,130 +29,138 @@ import net.minecraft.world.World;
 
 public class EntityGhostPlayer extends EntityMob {
 
-	private static final DataParameter<String> PLAYER_UUID = EntityDataManager
-			.<String> createKey(EntityGhostPlayer.class, DataSerializers.STRING);
+    private static final DataParameter<String> PLAYER_UUID = EntityDataManager
+            .<String>createKey(EntityGhostPlayer.class, DataSerializers.STRING);
 
-	public EntityGhostPlayer(World worldIn, UUID playerUUID, String playerName) {
-		this(worldIn);
+    public EntityGhostPlayer(World worldIn, UUID playerUUID, String playerName) {
+        this(worldIn);
 
-		this.setPlayerUUID(playerUUID);
-		this.setCustomNameTag(playerName);
-	}
+        this.setPlayerUUID(playerUUID);
+        this.setCustomNameTag(playerName);
+    }
 
-	public EntityGhostPlayer(World worldIn) {
-		super(worldIn);
-		this.setAlwaysRenderNameTag(false);
-		this.setSize(0.6F, 1.95F);
-	}
+    public EntityGhostPlayer(World worldIn) {
+        super(worldIn);
+        this.setAlwaysRenderNameTag(false);
+        this.setSize(0.6F, 1.95F);
+    }
 
-	@Override
-	public boolean getAlwaysRenderNameTagForRender() {
-		return getAlwaysRenderNameTag();
-	}
+    @Override
+    public boolean getAlwaysRenderNameTagForRender() {
+        return getAlwaysRenderNameTag();
+    }
 
-	@Override
-	protected void initEntityAI() {
-		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.0D, false));
-		this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
-		this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
-		this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-		this.tasks.addTask(8, new EntityAILookIdle(this));
-		
-		this.targetTasks.addTask(10, new EntityAINearestAttackableTarget<EntityPlayer>(this, EntityPlayer.class, true));
+    @Override
+    protected void initEntityAI() {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIAttackMelee(this, 1.0D, false));
+        this.tasks.addTask(5, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(7, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(8, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(9, new EntityAILookIdle(this));
 
-	}
+        if (Config.friendlyGhost) {
+            this.targetTasks.addTask(10, new EntityAINearestAttackableTarget<>(this, EntityLiving.class, 10, false, true, new Predicate<EntityLiving>() {
+                public boolean apply(@Nullable EntityLiving entityLiving) {
+                    return entityLiving != null && IMob.VISIBLE_MOB_SELECTOR.apply(entityLiving) && !(entityLiving instanceof EntityCreeper) && !(entityLiving instanceof EntityGhostPlayer);
+                }
+            }));
+        } else {
+            this.targetTasks.addTask(10, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
+        }
 
-	protected void applyEntityAttributes() {
-		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23000000417232513D);
-		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(2.0D);
-	}
+    }
 
-	protected void entityInit() {
-		super.entityInit();
-		this.getDataManager().register(PLAYER_UUID, new UUID(0, 0).toString());
-	}
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23000000417232513D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(3.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.ARMOR).setBaseValue(2.0D);
+    }
 
-	@Override
-	public boolean isEntityUndead() {
-		return true;
-	}
+    protected void entityInit() {
+        super.entityInit();
+        this.getDataManager().register(PLAYER_UUID, new UUID(0, 0).toString());
+    }
 
-	public EnumCreatureAttribute getCreatureAttribute() {
-		return EnumCreatureAttribute.UNDEAD;
-	}
+    @Override
+    public boolean isEntityUndead() {
+        return true;
+    }
 
-	@Nullable
-	protected ResourceLocation getLootTable() {
-		return null;
-	}
+    public EnumCreatureAttribute getCreatureAttribute() {
+        return EnumCreatureAttribute.UNDEAD;
+    }
 
-	public void setPlayerUUID(UUID uuid) {
-		this.getDataManager().set(PLAYER_UUID, uuid.toString());
-		if(uuid.toString().equals("af3bd5f4-8634-4700-8281-e4cc851be180")){
-			setOverpowered();
-		}
-	}
-	
-	private void setOverpowered(){
-		this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.4D);
-		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(20.0D);
-	}
-	
-	@Override
-	public void setCustomNameTag(String name) {
-		super.setCustomNameTag(name);
-		
-		if(name.equals("henkelmax")){
-			setOverpowered();
-		}
-	}
+    @Nullable
+    protected ResourceLocation getLootTable() {
+        return null;
+    }
 
-	public UUID getPlayerUUID() {
-		String uuidStr = this.getDataManager().get(PLAYER_UUID);
-		UUID uuid = new UUID(0, 0);
+    public void setPlayerUUID(UUID uuid) {
+        this.getDataManager().set(PLAYER_UUID, uuid.toString());
+        if (uuid.toString().equals("af3bd5f4-8634-4700-8281-e4cc851be180")) {
+            setOverpowered();
+        }
+    }
 
-		try {
-			uuid = UUID.fromString(uuidStr);
-		} catch (Exception e) {
+    private void setOverpowered() {
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(35.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.4D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(20.0D);
+    }
 
-		}
+    @Override
+    public void setCustomNameTag(String name) {
+        super.setCustomNameTag(name);
 
-		return uuid;
-	}
+        if (name.equals("henkelmax")) {
+            setOverpowered();
+        }
+    }
 
-	@Override
-	public void writeEntityToNBT(NBTTagCompound compound) {
-		super.writeEntityToNBT(compound);
-		compound.setString("player_uuid", getPlayerUUID().toString());
-	}
+    public UUID getPlayerUUID() {
+        String uuidStr = this.getDataManager().get(PLAYER_UUID);
+        UUID uuid = new UUID(0, 0);
 
-	@Override
-	public void readEntityFromNBT(NBTTagCompound compound) {
-		super.readEntityFromNBT(compound);
+        try {
+            uuid = UUID.fromString(uuidStr);
+        } catch (Exception e) {
 
-		if (compound.hasKey("player_uuid")) {
-			String uuidStr = compound.getString("player_uuid");
+        }
 
-			try {
-				UUID uuid = UUID.fromString(uuidStr);
-				setPlayerUUID(uuid);
-			} catch (Exception e) {
-			}
-		}
-	}
-	
-	@Override
-	public boolean attackEntityAsMob(Entity entityIn) {
-		if(entityIn.getName().equals("henkelmax")||entityIn.getUniqueID().toString().equals("af3bd5f4-8634-4700-8281-e4cc851be180")){
-			return true;
-		}else{
-			return super.attackEntityAsMob(entityIn);
-		}
-	}
+        return uuid;
+    }
+
+    @Override
+    public void writeEntityToNBT(NBTTagCompound compound) {
+        super.writeEntityToNBT(compound);
+        compound.setString("player_uuid", getPlayerUUID().toString());
+    }
+
+    @Override
+    public void readEntityFromNBT(NBTTagCompound compound) {
+        super.readEntityFromNBT(compound);
+
+        if (compound.hasKey("player_uuid")) {
+            String uuidStr = compound.getString("player_uuid");
+
+            try {
+                UUID uuid = UUID.fromString(uuidStr);
+                setPlayerUUID(uuid);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn) {
+        if (entityIn.getName().equals("henkelmax") || entityIn.getUniqueID().toString().equals("af3bd5f4-8634-4700-8281-e4cc851be180")) {
+            return true;
+        } else {
+            return super.attackEntityAsMob(entityIn);
+        }
+    }
 
 }
